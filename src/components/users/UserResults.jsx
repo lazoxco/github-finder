a5c63a7144fc4f84b9ee7c612125a99eb76981ac
github username <github email address>
2022-02-22 21:20:58 -0500

    Removed token.

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -11,11 +11,12 @@ function UserResults() {
   }, [])
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      }
-    })
+    // const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    //   headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    //   }
+    // })
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
 
     const data = await response.json()
 
