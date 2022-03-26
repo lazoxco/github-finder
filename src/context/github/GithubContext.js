053303b56df1293759aa9e66f79a4e4e64203647
github username <github email address>
2022-03-26 19:12:50 -0400

    Refactored fetch users.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -7,15 +7,16 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
+  // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading()
 
     const response = await fetch(`${GITHUB_URL}/users`)
-
     const data = await response.json()
 
     dispatch({
@@ -23,6 +24,9 @@ export const GithubProvider = ({children}) => {
       payload: data,
     })
   }
+
+  // Set loading
+  const setLoading = () => dispatch({type: 'SET_LOADING'})
 
   return <GithubContext.Provider value={{
     users: state.users,
