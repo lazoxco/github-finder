e708723c556d92b7cbe9d7a7006486ebdfe20f96
Nory Lazo <nlazo@me.com>
2022-06-30 09:25:01 -0400

    Adds alert to App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import {GithubProvider} from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
+          <Alert />
           <main className='container mx-auto px-3 pb-12'>
             <Routes>
               <Route path='/' element={<Home />} /> 
